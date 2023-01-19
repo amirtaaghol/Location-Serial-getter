99d9caa9220711ac6fffa8757534d2b48850b455
Amir Taaghol <amirtaaghol81@gmail.com>
2023-01-19 22:58:23 +0330

    Bug fixes

--- app/src/main/java/com/yarab/hamtashariftestproject/MainActivity.java
+++ app/src/main/java/com/yarab/hamtashariftestproject/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -15,7 +14,6 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Looper;
 import android.provider.Settings;
-import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,8 +21,6 @@ import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -32,10 +28,6 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.CancellationToken;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnTokenCanceledListener;
-import com.google.android.gms.tasks.Task;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -44,7 +36,7 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btn_loc, btn_serial, btn_submit;
-    TextView tv_loc, tv_serial;
+    TextView tv_loc_alt, tv_loc_long, tv_serial;
 
     final int REQ_LOC = 101;
     final int REQ_SERIAL = 102;
@@ -52,16 +44,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     FusedLocationProviderClient fusedLocationProviderClient;
 
     String serial;
-    String altitude ;
-    String longitude ;
+    String altitude;
+    String longitude;
 
-    TelephonyManager telephonyManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //
         initViews();
     }
 
@@ -69,20 +60,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_loc = findViewById(R.id.btn_loc);
         btn_serial = findViewById(R.id.btn_serial);
         btn_submit = findViewById(R.id.btn_submit);
-        tv_loc = findViewById(R.id.tv_loc);
+        tv_loc_alt = findViewById(R.id.tv_loc_altitude);
+        tv_loc_long = findViewById(R.id.tv_loc_longitude);
         tv_serial = findViewById(R.id.tv_serial);
         //
         btn_loc.setOnClickListener(this);
         btn_serial.setOnClickListener(this);
         btn_submit.setOnClickListener(this);
         //
-        tv_loc.setText(" ");
-        tv_serial.setText(" ");
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
 
-    @SuppressLint("MissingPermission")
+    @SuppressLint({"MissingPermission", "HardwareIds"})
     @Override
     public void onClick(View v) {
         int id = v.getId();
@@ -90,19 +80,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (id == R.id.btn_loc) {
             if (hasLocPermission()) {
                 if (isLocationEnabled()) {
-                    fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Location> task) {
-                            Location location = task.getResult();
-                            if (location == null) {
-                                requestNewLocation();
-                            } else {
-                                tv_loc.setText("Location : ");
-                                altitude = String.valueOf(location.getAltitude());
-                                tv_loc.append("\nAltitude : " + altitude);
-                                longitude = String.valueOf(location.getLongitude());
-                                tv_loc.append("\nLongitude : " + longitude);
-                            }
+                    fusedLocationProviderClient.getLastLocation().addOnCompleteListener(task -> {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocation();
+                        } else {
+                            altitude = String.valueOf(location.getAltitude());
+                            tv_loc_alt.setText("Altitude : " + altitude);
+                            longitude = String.valueOf(location.getLongitude());
+                            tv_loc_long.setText("Longitude : " + longitude);
                         }
                     });
                 } else {
@@ -117,14 +103,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 //                telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
 //                serial = telephonyManager.getImei();
+
             serial = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
-            tv_serial.setText("Serial Number :");
-            tv_serial.append(serial);
+            tv_serial.setText(serial);
 
 
         } else if (id == R.id.btn_submit) {
 
-            if ((tv_loc.getText().toString().trim().isEmpty()) && (tv_serial.getText().toString().trim().isEmpty())) {
+            if ((tv_loc_alt.getText().toString().trim().isEmpty()) && (tv_serial.getText().toString().trim().isEmpty())) {
                 Toast.makeText(this, "لطفا با کلیک بر روی دکمه ها اطللاعات را دریافت کنید", Toast.LENGTH_SHORT).show();
             } else {
                 JSONArray data = new JSONArray();
@@ -141,24 +127,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
 
                 RequestQueue requestQueue = Volley.newRequestQueue(this);
-                String url = "https://demo.thingsboard.io:1881/api/v1/$G4yFFyaE295WKMa8KzZK/telemetry";
+                String url = "http://demo.thingsboard.io:443/api/v1/G4yFFyaE295WKMa8KzZK/telemetry";
 
                 JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST,
-                        url, data, new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        try {
-                            Toast.makeText(MainActivity.this, "response is : " + response.getString(0), Toast.LENGTH_SHORT).show();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
+                        url, data, response -> {
+                            try {
+                                Toast.makeText(MainActivity.this, "response is : " + response.getString(1), Toast.LENGTH_SHORT).show();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }, error -> {
 
-                    }
-                });
+                        });
                 requestQueue.add(jsonArrayRequest);
                 Toast.makeText(this, "داده ها با موفقیت ارسال شدند.", Toast.LENGTH_SHORT).show();
 
@@ -188,11 +168,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public void onLocationResult(LocationResult locationResult) {
             Location mLastLocation = locationResult.getLastLocation();
-            tv_loc.setText("Location : ");
+
             altitude = String.valueOf(mLastLocation.getAltitude());
-            tv_loc.append("\nAltitude : " + altitude);
+            tv_loc_alt.append("\t" + altitude);
             longitude = String.valueOf(mLastLocation.getLongitude());
-            tv_loc.append("\nLongitude : " + longitude);
+            tv_loc_long.append("\t" + longitude);
         }
     };
 
@@ -212,12 +192,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             new AlertDialog.Builder(this).setCancelable(false)
                     .setMessage("برای دریافت موقعیت مکانی نیاز به دسترسی می باشد")
-                    .setPositiveButton("قبول", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            requestLocPermission();
-                        }
-                    }).show();
+                    .setPositiveButton("قبول", (dialog, which) -> requestLocPermission()).show();
         }
         return false;
     }
@@ -237,12 +212,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             new AlertDialog.Builder(this).setCancelable(false)
                     .setMessage("برای خواندن سریال نیاز به دسترسی می باشد")
-                    .setPositiveButton("قبول", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            requestSerialPermission();
-                        }
-                    }).show();
+                    .setPositiveButton("قبول", (dialog, which) -> requestSerialPermission()).show();
         }
         return false;
     }
